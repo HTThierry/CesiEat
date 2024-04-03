a4bfa74c2745dc74d80d7a56c8e5f66967b0c707
Mamadou Barry <mamadou.barry2518@gmail.com>
2024-04-03 16:28:45 +0200

    Added initial models

---
+++ backend/models/userModel.js
@@ -0,0 +1,68 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    match: /.+\@.+\..+/,
+    unique: true,
+    trim: true,
+    lowercase: true
+  },
+  phone: {
+    type: String,
+    required: true,
+    trim: true,
+    validate: {
+      validator: function(v) {
+        return /\d{10}/.test(v);
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    }
+  },
+  age: {
+    type: Number,
+    required: true
+  },
+  address: {
+    type: String,
+    required: true
+  },
+  city: {
+    type: String,
+    required: true
+  },
+  postalCode: {
+    type: Number,
+    required: true,
+    validate: {
+      validator: function(v) {
+        return /\d{5}/.test(v); // Adjust the regex based on the country's postal code format
+      },
+      message: props => `${props.value} is not a valid postal code!`
+    }
+  },
+  resetPasswordToken: String,
+  resetPasswordExpires: Date
+}, { timestamps: true });
+
+// Handle duplicate email errors
+userSchema.post('save', function(error, doc, next) {
+  if (error.name === 'MongoServerError' && error.code === 11000) {
+    next(new Error('Email already exists.'));
+  } else {
+    next(error);
+  }
+});
+
+module.exports = mongoose.model('userModel', userSchema);
