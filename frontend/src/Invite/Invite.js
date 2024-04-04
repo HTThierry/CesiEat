46ab9ba6b2d700d38ab7e07eb9570cab534f643a
LukenIrazoqui <123357726+LukenIrazoqui@users.noreply.github.com>
2024-04-04 09:08:42 +0200

    icons deleted, can us react icons, invite page finished

--- frontend/src/Invite/Invite.js
+++ frontend/src/Invite/Invite.js
@@ -2,22 +2,80 @@ import React, { useState } from 'react';
 import './Invite.css';
 import BlackHeader from "../Headers/BlackHeader";
 import Footer from "../Footer/Footer";
+import { FaCopy, FaRegCopy } from 'react-icons/fa';
+import Navbar from "../Main/Navbar/Navbar";
 
 const Invite = () => {
+
+    const [copiedIndex, setCopiedIndex] = useState(null);
+
+    const codes = [
+        { label: 'Code client', code: 'XXXXXX' },
+        { label: 'Code livreur', code: 'XXXXXX' },
+        { label: 'Code restaurateur', code: 'XXXXXX' }
+    ];
+
+/*
+    const [codes, setCodes] = useState([]);
+
+    useEffect(() => {
+        fetch('/api/codes')
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                }
+                throw new Error('Network response was not ok.');
+            })
+            .then(data => {
+                setCodes(data);
+            })
+            .catch(error => {
+                console.error('Error fetching codes:', error);
+            });
+    }, []);
+*/
+
+    const copyToClipboard = (code, index) => {
+        navigator.clipboard.writeText(code).then(() => {
+            setCopiedIndex(index);
+
+            setTimeout(() => {
+                setCopiedIndex(-1);
+            }, 1000);
+        }, () => {
+            console.error('Failed to copy code');
+        });
+    };
+
     return (
         <div className="invite-page CesiEatsMedium">
-            <BlackHeader/>
+            <BlackHeader
+                leftIcons={
+                    <div className="left-icons">
+                        <Navbar/>
+                    </div>
+                }
+            />
             <div className="container">
                 <h2>Votre/vos code(s) de parrainage :</h2>
-                <label
-                    className="input"
-                />
-                <label
-                    className="input"
-                />
-                <label
-                    className="input"
-                />
+                <div>
+                    {codes.map((item, index) => (
+                        <div key={index}
+                             className="code">
+                            <span>{item.label}: {item.code}</span>
+                            <div className="copy-container">
+                                <button onClick={() => copyToClipboard(item.code, index)}
+                                        title="Copier le code"
+                                        style={{backgroundColor: 'transparent', border: 'none'}}>
+                                    {copiedIndex === index ? <FaCopy/> : <FaRegCopy/>}
+                                </button>
+                                {copiedIndex === index && (
+                                    <div className="copied-text">Copié !</div>
+                                )}
+                            </div>
+                        </div>
+                    ))}
+                </div>
             </div>
             <div className="backgroundLeft"></div>
             <div className="backgroundRight"></div>
